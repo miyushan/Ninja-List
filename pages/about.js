c0b69643644a54412d3cc302045a7b66591d1baf
Miyushan <smiushan2015@gmail.com>
2022-02-01 23:42:39 +0530

    fetching data

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,19 @@
+import Head from 'next/head';
+
 const About = () => {
-    return ( 
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error vitae, adipisci tempore ex vel, atque, illum nam pariatur quod sequi eius impedit? Ratione fugit earum corrupti enim accusantium ea dolore?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error vitae, adipisci tempore ex vel, atque, illum nam pariatur quod sequi eius impedit? Ratione fugit earum corrupti enim accusantium ea dolore?</p>
-        </div>
-     );
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error vitae, adipisci tempore ex vel, atque, illum nam pariatur quod sequi eius impedit? Ratione fugit earum corrupti enim accusantium ea dolore?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error vitae, adipisci tempore ex vel, atque, illum nam pariatur quod sequi eius impedit? Ratione fugit earum corrupti enim accusantium ea dolore?</p>
+            </div>
+        </>
+    );
 }
  
 export default About;
