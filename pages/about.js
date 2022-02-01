486d0404f280c092e353e6869762002f6eee9385
Miyushan <smiushan2015@gmail.com>
2022-02-01 22:57:49 +0530

    redirrect

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error vitae, adipisci tempore ex vel, atque, illum nam pariatur quod sequi eius impedit? Ratione fugit earum corrupti enim accusantium ea dolore?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error vitae, adipisci tempore ex vel, atque, illum nam pariatur quod sequi eius impedit? Ratione fugit earum corrupti enim accusantium ea dolore?</p>
+        </div>
+     );
+}
+ 
+export default About;
